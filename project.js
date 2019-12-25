b80f733ed62dc82b6e05dc0ed56cc2b59d45c252
TYSHI123 <41343711+TYSHI123@users.noreply.github.com>
2019-12-25 21:38:26 +0800

    Add files via upload

---
+++ project.js
@@ -0,0 +1,13 @@
+
+
+// function getMousePos(event){
+//     document.write(111111);
+//     var e = event || window.event;
+//     $(document).ready(function(){
+//         $("*").click(function(){
+//             $("#div1").fadeOut(400);
+//             $("#div2").delay(1000).fadeIn("slow");
+//         });
+//     });
+//     return screenX,screenY;
+// }
